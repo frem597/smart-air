5cd01dde26107cbdc21373a009989a164f298a49
frem597 <ktqnx8z2b7@privaterelay.appleid.com>
2025-12-16 19:21:09 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,8 +15,8 @@ function tempUp() {
   fetch(db + "control/temp.json")
     .then(res => res.json())
     .then(temp => {
-      temp = (temp ?? 25) + 1;   // โค้ดเดิม
-      if (temp > 30) temp = 30; // 🔹 แทรก: จำกัดค่าสูงสุด
+      temp = (temp ?? 25) + 1;
+      if (temp > 30) temp = 30;
 
       fetch(db + "control/temp.json", {
         method: "PUT",
@@ -32,8 +32,8 @@ function tempDown() {
   fetch(db + "control/temp.json")
     .then(res => res.json())
     .then(temp => {
-      temp = (temp ?? 25) - 1;   // โค้ดเดิม
-      if (temp < 16) temp = 16; // 🔹 แทรก: จำกัดค่าต่ำสุด
+      temp = (temp ?? 25) - 1;
+      if (temp < 16) temp = 16;
 
       fetch(db + "control/temp.json", {
         method: "PUT",
@@ -44,24 +44,17 @@ function tempDown() {
     });
 }
 
-// โหลดค่าเมื่อเปิดหน้า
 window.onload = loadTemp;
 
-/* ===== แทรก Real-time listener ===== */
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
-<script>
-  // config Firebase
-  var firebaseConfig = {
-    databaseURL: "https://esp32-a511e-default-rtdb.asia-southeast1.firebasedatabase.app"
-  };
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+// ===== Real-time listener =====
+var firebaseConfig = {
+  databaseURL: "https://esp32-a511e-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-  // ฟังค่าอุณหภูมิแบบ Real-time
-  database.ref("control/temp").on("value", (snapshot) => {
-    let temp = snapshot.val();
-    if (temp === null) temp = 25;
-    document.getElementById("tempValue").innerText = temp;
-  });
-</script>
+database.ref("control/temp").on("value", (snapshot) => {
+  let temp = snapshot.val();
+  if (temp === null) temp = 25;
+  document.getElementById("tempValue").innerText = temp;
+});
